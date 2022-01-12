92d1928ecf39b50ba898dd7b5cd3dfea6b147e63
jujucheck <57398219+jujucheck@users.noreply.github.com>
2022-01-12 17:32:31 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,10 +1,114 @@
 var member = [
   {
-
+    "phone": "No.2038",
     "name": "星野冥一"
   },
   {
-    
+    "phone": "No.0282",
     "name": "Mr.Q"
+  },
+  {
+    "phone": "No.3392",
+    "name": "一般人类"
+  },
+  {
+    "phone": "No.8080",
+    "name": "萝莉捕捉者"
+  },
+  {
+    "phone": "No.3855",
+    "name": "四代猫愿"
+  },
+  {
+    "phone": "No.6389",
+    "name": "人形大魔王"
+  },
+  {
+    "phone": "No.4440",
+    "name": "痴言心醉"
+  },
+  {
+    "phone": "No.3170",
+    "name": "渣瓜一隻"
+  },
+  {
+    "phone": "No.1688",
+    "name": "住之江圭太"
+  },
+  {
+    "phone": "No.0018",
+    "name": "九条鱼卡"
+  },
+  {
+    "phone": "No.0318",
+    "name": "nightor"
+  },
+  {
+    "phone": "No.7712",
+    "name": "井下落石"
+  },
+  {
+    "phone": "No.6561",
+    "name": "埃尔o妮娅"
+  },
+  {
+    "phone": "No.1260",
+    "name": "Sapha"
+  },
+  {
+    "phone": "No.6280",
+    "name": "西行寺蓝蓝路"
+  },
+  {
+    "phone": "No.8517",
+    "name": "堀江由衣"
+  },
+  {
+    "phone": "No.2335",
+    "name": "十万巫女"
+  },
+  {
+    "phone": "No.2681",
+    "name": "Halu"
+  },
+  {
+    "phone": "No.4024",
+    "name": "lupin"
+  },
+  {
+    "phone": "No.0463",
+    "name": "太阳主宰"
+  },
+  {
+    "phone": "No.2423",
+    "name": "alkd"
+  },
+  {
+    "phone": "No.5357",
+    "name": "Allenz"
+  },
+  {
+    "phone": "No.8751",
+    "name": "达也的背影"
+  },
+  {
+    "phone": "No.0737",
+    "name": "神隐少女"
+  },
+  {
+    "phone": "No.5959",
+    "name": "尐疯寳児不尐了"
+  },
+  {
+    "phone": "No.8909",
+    "name": "⌒袶醽之子↓"
+  },
+  {
+    "phone": "No.8441",
+    "name": "我家的二喵"
+  },
+  {
+    "phone": "No.1746",
+    "name": "维他酱酱"
   }
 ]
